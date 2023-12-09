d41f2690b851b69740a2bb9f32b05a5134e046c5
Douglas Rocha <douglas_igt14@hotmail.com>
2023-12-09 15:24:43 -0300

    WIP

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::middleware('auth:api')->group(function () {
+    // Rotas protegidas pelo middleware
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
