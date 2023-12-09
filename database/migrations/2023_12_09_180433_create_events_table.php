00044e6ed5fbc968d80ee70ce9cb3254ee210730
Douglas Rocha <douglas_igt14@hotmail.com>
2023-12-09 15:11:26 -0300

    WIP

--- database/migrations/2023_12_09_180433_create_events_table.php
+++ database/migrations/2023_12_09_180433_create_events_table.php
@@ -4,24 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateEventsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->dateTime('data_inicio');
+            $table->dateTime('data_fim');
+            $table->boolean('status');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('events');
     }
-};
+}
--- database/migrations/2023_12_09_180441_create_inscritos_table.php
+++ database/migrations/2023_12_09_180441_create_inscritos_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateInscritosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('inscritos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->string('cpf');
+            $table->string('email');
+            $table->foreignId('event_id')->constrained('events');
+            // Adicione outros campos, se necessário
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('inscritos');
     }
-};
+}
